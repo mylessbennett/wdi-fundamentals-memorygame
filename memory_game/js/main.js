efbadb390d7b086b8ad222163712fc769de5c3e0
Myles Bennett <mylesbennett@Myles-MacBook-Pro.local>
2019-01-28 01:32:56 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,19 @@
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+ var cardOne = cards[0];
+ var cardTwo= cards[1];
 
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You Found a Match.");
+	} else {
+		alert("Sorry, Try Again.");
+	}
+};
+
+console.log("User flipped " + cardOne);
 console.log("User flipped " + cardTwo);
-console.log("User flipped " + cardThree);
