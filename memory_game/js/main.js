6ea20cfc79dd0f097f5afc1a258fb930fb26169a
Myles Bennett <mylesbennett@Myles-MacBook-Pro.local>
2019-01-30 00:37:46 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -21,7 +21,30 @@ var cards = [
 	}
 
 ];
+
+var createBoard = function() {
+	for (i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
 var cardsInPlay = [];
+
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id')
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}; 
+};
 
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -31,15 +54,5 @@ var checkForMatch = function() {
 	};
 };
 
-var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
-	cardsInPlay.push(cards[cardId].rank);
-	if (cardsInPlay.length === 2) {
-		checkForMatch();
-	}; 
-};
+createBoard();
 
-flipCard(0);
-flipCard(2);
